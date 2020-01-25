3f8a70db402a06f731a65465b39e7f9bb168c188
jkoehler <joshua.koehler86@gmail.com>
2020-01-24 20:49:14 -0800

    Modularized, wrote getVelocity using altimeter data

--- airbrakes.ino
+++ airbrakes.ino
@@ -1,19 +1,38 @@
+// Joshua Köhler - 1/24/20 - Clark Aerospace - ESRA 2020 - Airbrakes Software
+
 //*TODO*
-/*	1. Ensure vertical velocity function is working properly
+/*	1. Write vertical velocity function
 		2. Check units of all sensors
-		2. Modularize functions
+		3. Modularize functions
+
+Also a few optimization decisions to make:
+1.	Should each get____ function get a new event, or should they all be passed
+		the same event for consistency?
+
+2.	Is using the system time accurate enought to compute velocity with the
+		altimeter?
+
+3.	If #2 is good, should we use the same method to compute accleration instead
+		of the accelerometer?
+
+4.	Consider the system timing of the PID, and how using our own timing in the 
+		velocity function could affect this.
+
+5.  Add code to turn off the airbrakes after a successful flight
+
+6. Make sure the max for the analogWrite, hardware limit switches, and PID limit		are all in sync.  Default PID limit should be 255 (2^8 -1)
+
 */
 
-
 // CHECK UNITS OF ALL SENSORS TO ENSURE WE HAVE THE RIGHT UNITS!
-
 #define g 9.80665 //acceleration of gravity (m/s^2)
 #define airbrakeMotor 8 //pin to control motor
 #define halfwayToApogee 1524 //1524m = 5000ft
 #define apogeeHeight 3048 //3048m = 10,000ft
-#include<stdio.h>
+#include <stdio.h>
 #include <math.h> //for trig functions
 #include <PID_v1.h> //PID library
+#include <time.h> //used to get system time for calculations
 #include <Servo.h> //library to control airbrake servo motor
 #include <Wire.h>
 #include <Adafruit_MPL3115A2.h> //Altimeter library
@@ -26,19 +45,14 @@ Adafruit_BNO055 bno = Adafruit_BNO055(55);
 //instantiates new barometer class with name baro
 Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
 
-//Define variables as global - could use pointers for memory reduction
-double Setpoint, Input, Output, alt, v, a, m, rawv,rawa, previous,Cd,theta;
+//Define variables as  - could use pointers for memory reduction
+double Setpoint, Input, Output; //global variables 
 //Specify the links and initial tuning parameters
 PID myPID(&Input, &Output, &Setpoint,2,5,1, REVERSE);
 
-/* Arduino automatically does function prototypes
-double getDrag(a,v); //Prototype
-double getProjectedAltitude(Cd, v, alt); //Prototype
-double VerticalAcceleration(); //Prototype
-double VerticalVelocity(); //Prototype
-double getAngleToVertical(); //Prototype
-*/
-void setup() {
+// Arduino automatically does function prototypes
+
+void setup() {//all code in the setup function is run only once
   Servo esc; //declare electronic speed controller
   
 	pinMode(7,INPUT);//For sensors?
@@ -47,8 +61,8 @@ void setup() {
   esc.attach(airbrakMotor); //connects signal wire to pin 8 to control the motor
   bno.setExtCrystalUse(true); //use external crystal boolean (keeps track of time
  
-	double apogee=apogeeHeight+baro.getAltitude(); //3048 meters=10,000 ft. this variable will never change, it is our setpoint (where we want the rocket to nose over at)
-  Setpoint=apogee; // This should be correct, this way the rocket is always shooting for exactly 10k above ground
+	Setpoint = apogeeHeight+baro.getAltitude(); //3048 meters=10,000 ft. this variable will never change, it is our setpoint (where we want the rocket to nose over at)
+// This should be correct, this way the rocket is always shooting for exactly 10k above ground
 
   // PID Set-Up - Default max output is 255, Default were specified above @ 2,5,1
   // We should compare these to see how they fare, collecting data of projected altitude vs. setpoint over time
@@ -56,58 +70,61 @@ void setup() {
 
 //This next chunk of code waits until after motor-burnout to start the airbrakes algorithm
 // We could probably do this with while(alt-Setpoint<5000) {alt=baro.getAltitude}
-
+	double alt=0;//initialize alt to force while loop
   while(alt-Setpoint < halfwayToApogee) {//1524meters = 5,000ft (half-way to apogee)
-    v=VerticalVelocity();
-    a=VerticalAcceleration();
-    alt=baro.getAltitude;//keep measuring the altitude till motor burnout 
+    alt=baro.getAltitude();//keep measuring the altitude till motor burnout 
   }
-
-  Cd = getDrag(a,v);
-  double alt = baro.getAltitude();
-  Input = getProjectedAltitude(Cd, v, alt);
-  // turn the PID on
-  myPID.SetMode(AUTOMATIC);
+	setInput();//set Input before the PID gets turned on to eliminate weirdness 
+	myPID.SetMode(AUTOMATIC);//turn the PID on
 }
+
+/*
+This main function loop has three steps
+1. Read sensors, and calculate the Input variable for the PID compute.
+2. Compute Output with the PID
+3. Write this output to the airbrakes motor
+*/
 void loop() {
-  // Setpoint is permanently set, so all we need is input
+  setInput();//set Input for PID compute
 
-  /* Get a new sensor event */
-  // Sensors
-  sensors_event_t event; //initializing struct of type sensors_event_t
-  bno.getEvent(&event); //storing the sensor event in class event (e.g get data)
-  rawa = event.acceleration.z; //get z acceleration *Z IS WITH RESPECT TO GYRO NOT ROCKET
-
-  //*TODO* get raw velocity and store in rawv
-  v=VerticalVelocity(rawv,event);//computes velocity with respect to true vertical
-  
-	//throw these next three lines in a UDF entitled getRead();
-	rawa = event.acceleration.z;
-  a=VerticalAcceleration(rawa, event);
-  alt = baro.getAltitude();
-  Cd=getDrag(a,v);
-  double Input = getProjectedAltitude(Cd, v, alt);//Input is projected alt
-  
-	// our error is: error = Setpoint - Input;
-  // this computes the PID and stores the needed output as global variable <Output>
   myPID.Compute();//have the PID compute the proper output
 
   // Probably should set an analog out limit in the PID software
 	//write the computed output to the airbrakeMotor pin
-	analogWrite(airbrakeMotor,Output); }
+	analogWrite(airbrakeMotor,Output);
+}
+
 
 /*
-This function takes two inputs, the vertical acceleration (a), and the 
-velocity (v).
+Gets all readings to calculate the Input for the PID.
+Note that Input is a global variable, so the PID function can access it at 
+all times.
+*/
+void setInput(void){
+	sensors_t_event event; //initialize for gyro read
+	double alt,v,Cd;
+	bno.getEvent(&event);//get data from the gyro
+  alt = baro.getAltitude();//get data from altimeter
+	v = getVelocity();//get velocity from altimeter THIS PAUSES THE ARDUINO
+  Cd = getDrag(event,v);
+  Input = getProjectedAltitude(Cd, v, alt);//calculate Input to PID
+}
+/*
+This function takes two inputs, an event of type sensors_t_event
+for the gyro sensor, and the velocity (v).
 
 Using these two variables, it will compute the approximate drag coefficient 
 of the rocket in real time.  
+
+Note that getAcceleration(event) is called inside of get drag in order to
+compute the vertical acceleration.
 
 Because drag depends on how fully extended the airbrake flaps are, we must
 continuously calculate drag as we make corrections based off of our 
 projected altitude.
 */
-double getDrag(double a, double v) {
+double getDrag(sensors_t_event event,double v) {
+	double a = getAcceleration(event);
   double c = (a-m*g)/(v*v); // from mg+cv^2=a, we rearrange to solve for c
   return c;
 }
@@ -131,27 +148,34 @@ double getProjectedAltitude(double Cd,double v,double alt) {
 }
 
 /*
-Computes vertical acceleration given rawacc (raw acceleration) which is
-the acceleration relative to the z-axis
-OF THE ROCKET (we are concerned with the z-axis that is relative to earth)
+Computes vertical acceleration relative to earth.
+event.acceleration.z gives the vertical acceleration relative to the 
+NOSE OF THE ROCKET (we are concerned with the z-axis that is relative to earth)
+The angle theta is used to compute the vertical acceleration relative to earth.
 event is a structure of type sensors_event_t that is passed to getAngletoVertical
 */
-double VerticalAcceleration(double rawacc,sensors_event_t event) {//event is a struct of type sensors_event_t
+double getAcceleration(sensors_event_t event) {//event is a struct of type sensors_event_t
   double theta = getAngleToVertical(event); //get from hardware
   //using spherical coordinates
-  double a = cos(theta)*rawacc;
+  double a = cos(theta)*event.acceleration.z;
   return a;
 }
 
 /*
-rawvelocity is the velocity in the z direction OF THE GYROSCOPE NOT THE ROCKET
-event is a structure of type sensors_event_t that is passed to getAngletoVertical
+This function uses the difference between two altimeter readings, with a timed
+pause in-between, to calculate velocity.  We are not sure how accurate this will
+be.
 */
-double VerticalVelocity(double rawvelocity, sensors_event_t event) {
-  double theta = getAngleToVertical(event); //get from hardware
-  //using spherical coordinates
-  double v = cos(theta)*rawvelocity;
-  return v;
+double getVelocity(void) {
+	time_t start,end;
+	double alt,theta,velocity;
+	start = time(NULL); //get begin time
+	alt = baro.getAltitude();//get first altitude reading 
+	sleep(0.1);//sleep for 100ms
+	alt = baro.get(altitude)-alt;//2nd reading - 1st reading is the difference in altitude over the time period
+	end = time(NULL);//get finish time
+	velocity=alt/(end-start);//velocity is distance/time
+  return velocity;
 }
 
 /*
@@ -163,9 +187,9 @@ double getAngleToVertical(sensors_event_t event) {
   // hardware read
   double x=event.orientation.x;//get angle to x
   double y=event.orientation.y;//get angle to y
-  double z=event.orientation.z;//get angle to z
+  double angle to z
 	
 	// conversion to spherical
-  double theta=atan(y/x);//rectangular to spherical conversion
+   theta=atan(y/x);//rectangular to spherical conversion
   return theta;
 }
