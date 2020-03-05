d137d0fd86ae3889592256795a39e0bf057e6098
jkoehler <joshua.koehler86@gmail.com>
2020-03-04 22:19:54 -0800

    Optimized velocity function

--- airbrakes.ino
+++ airbrakes.ino
@@ -24,6 +24,7 @@ Also a few optimization decisions to make:
 */
 
 #define gravity 9.80665 //acceleration of gravity (m/s^2)
+#define ACC_ID 55 //arbitrary id number of accelerometer sensor
 #define mass 60 //mass of the rocket *TODO* THIS MUST BE CHANGED TO BE ACCURATE!
 #define airbrakeMotor 8 //pin to control motor
 #define halfwayToApogee 1524 //1524m = 5000ft
@@ -37,42 +38,91 @@ Also a few optimization decisions to make:
 #include <Wire.h>
 #include <Adafruit_MPL3115A2.h> //Altimeter library
 #include <Adafruit_Sensor.h> //General sensor library for Adafruit
-#include <Adafruit_BNO055.h> //Gyro
+#include <Adafruit_BNO055.h> //Accelerometer
 #include <utility/imumaths.h>//for trig
 
+/*
+Pin connections map:
+
+Accelerometer:
+SCL - analog 5
+SDA - analog 4
+VDD - 3.3-5V DC
+GROUND - GND
+
+Barometric Altimeter:
+INPUT - analog 7
+
+Stepper motor:
+OUT - analog 8
+
+*/
+
+// Set the delay between fresh samples
+uint16_t BO055_SAMPLERATE_DELAY_MS = 100;
+
 //The function instantiates a new Gyroscope class with name bno
-Adafruit_BNO055 bno = Adafruit_BNO055(55);
+Adafruit_BNO055 bno = Adafruit_BNO055(ACC_ID);//ACC_ID is arbitary id
+
 //instantiates new barometer class with name baro
 Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
 
 double Setpoint, Input, Output; //global variables for PID
-//Specify the links and initial tuning parameters
+
+/*
+Specify the links and initial tuning parameters
+PID Set-Up - Default max output is 255, Default were specified above @ 2,5,1
+We should compare these to see how they fare, collecting data of 
+projected altitude vs. setpoint over time
+Default sample time is 200ms, which should be sufficient, tuning parameters 
+matter more.
+*/
 PID myPID(&Input, &Output, &Setpoint,2,5,1, REVERSE);
+
+double velocity = 0;  //Initialize global velocity variable to 0
 
 // Arduino automatically does function prototypes
 
 void setup() {//all code in the setup function is run only once
+	Serial.begin(115200);
+	Serial.println("Orientation Sensor Test"); Serial.println("");
+
+	if(!bno.begin()){
+		//There was a problem detecting the BNO055 ... check your connections
+		Serial.print("BNO055 not detected, check wiring or I2C address");
+		while(1);//display error message forever
+	}
+
+	delay(1000);//delay to display messages
+
   Servo esc; //declare electronic speed controller
 
-	pinMode(6,INPUT);//acceleromter
 	pinMode(7,INPUT);//altimeter 
 	pinMode(airbrakeMotor,OUTPUT);//airbrakeMotor is pin 8
 
   esc.attach(airbrakeMotor); //connects signal wire to pin 8 to control the motor
 	bno.setExtCrystalUse(true); //use external crystal boolean (keeps track of time
  
-	Setpoint = apogeeHeight+baro.getAltitude(); //3048 meters=10,000 ft. this variable will never change, it is our setpoint (where we want the rocket to nose over at)
-// This should be correct, this way the rocket is always shooting for exactly 10k above ground
+	/*
+	3048 meters=10,000 ft. this 
+	variable will never change, it is our setpoint (where we want the rocket to 
+	nose over at)
+	The PID is always aiming for 3048m
+	*/
+	Setpoint = apogeeHeight+baro.getAltitude(); 
 
-  // PID Set-Up - Default max output is 255, Default were specified above @ 2,5,1
-  // We should compare these to see how they fare, collecting data of projected altitude vs. setpoint over time
-  // Default sample time is 200ms, which should be sufficient, tuning parameters matter more
+	/*
+	This next chunk of code waits until an arbitary altitude to start the airbrakes
+	algorithm. More simulations should be done to optimize this with motor burn-out
+	and effectiveness of airbrakes at that time. A preset pre-burnout braking
+	algorithm depending on boost could be run before this PID code kicks in to
+	improve performance.
+	*/
 
-//This next chunk of code waits until after motor-burnout to start the airbrakes algorithm
-// We could probably do this with while(alt-Setpoint<5000) {alt=baro.getAltitude}
 	double alt=0;//initialize alt to force while loop
-  while(alt-Setpoint < halfwayToApogee) {//1524meters = 5,000ft (half-way to apogee)
-    alt=baro.getAltitude();//keep measuring the altitude till motor burnout 
+  //1524meters = 5,000ft (half-way to apogee)
+  while(alt-Setpoint < halfwayToApogee) {
+    alt=baro.getAltitude();//keep measuring the altitude 
   }
 	myPID.SetMode(AUTOMATIC);//turn the PID on
 }
@@ -88,8 +138,10 @@ void loop() {
 
   myPID.Compute();//have the PID compute the proper output
 
-	// Probably should set an analog out limit in the PID software
-	//write the computed output to the airbrakeMotor pin
+	/* 
+	Probably should set an analog out limit in the PID software
+	write the computed output to the airbrakeMotor pin
+	*/
 	analogWrite(airbrakeMotor,Output);
 }
 
@@ -100,17 +152,23 @@ Note that Input is a global variable, so the PID function can access it at
 all times.
 */
 void setInput(void){
-	sensors_event_t event; //initialize for gyro read
-	double alt,v,Cd;
-	bno.getEvent(&event);//get data from the gyro
-  alt = baro.getAltitude();//get data from altimeter
+	double alt,Cd;
+	
+	/* Note:
+	Consider getting one event of accelerometer, and one of gyro 
+	and passing them to all functions.  This should improve performance.
+	sensors_event_t accEvent, orientEvent;
+	bno.getEvent(&accEvent, Adaruit_BNO055::VECTOR_LINEARACCEL);//read acceleromete
+	bno.getEvent(&orientEvent, Adaruit_BNO055::VECTOR_GYROSCOPE);//read gyro
+  */
+	alt = baro.getAltitude();//get data from altimeter
 	v = getVelocity();//get velocity from altimeter THIS PAUSES THE ARDUINO
-  Cd = getDrag(event,v);
+  Cd = getDrag();
   Input = getProjectedAltitude(Cd, v, alt);//calculate Input to PID
 }
 /*
-This function takes two inputs, an event of type sensors_event_t
-for the gyro sensor, and the velocity (v).
+This function computes acceleration, storing it in a.
+It then calculates the drag coefficient, using the global velocity variable.
 
 Using these two variables, it will compute the approximate drag coefficient 
 of the rocket in real time.  
@@ -122,9 +180,10 @@ Because drag depends on how fully extended the airbrake flaps are, we must
 continuously calculate drag as we make corrections based off of our 
 projected altitude.
 */
-double getDrag(sensors_event_t event,double v) {
-	double a = getAcceleration(event);
-  double c = (a-mass*gravity)/(v*v); // from mg+cv^2=a, we rearrange to solve for c
+double getDrag(void) {
+	double a = getAcceleration();
+	// from mg+cv^2=a, we rearrange to solve for c
+  double c = (a-mass*gravity)/(velocity*velocity); 
   return c;
 }
 
@@ -153,29 +212,31 @@ NOSE OF THE ROCKET (we are concerned with the z-axis that is relative to earth)
 The angle theta is used to compute the vertical acceleration relative to earth.
 event is a structure of type sensors_event_t that is passed to getAngletoVertical
 */
-double getAcceleration(sensors_event_t event) {//event is a struct of type sensors_event_t
-  double theta = getAngleToVertical(event); //get from hardware
+double getAcceleration(void) {//event is a struct of type sensors_event_t
+	sensors_event_t event;//event variable
+	bno.getEvent(&event, Adafruit_BNO055::VECTOR_LINEARACCEL);//read sensor
+  double theta = getAngleToVertical(); //get from gyro
   //using spherical coordinates
   double a = cos(theta)*event.acceleration.z;
   return a;
 }
 
 /*
-This function uses the difference between two altimeter readings, with a timed
-pause in-between, to calculate velocity.  We are not sure how accurate this will
-be.
+This function uses the average of two accelerometer readings multiplied by
+the elapsed time, summed with the previous velocity reading.
+Thus it integrates over the acceleration to find velocity.
 
-Function should be changed to integrate acceleration to optimize performance.
+velocity is a global variable that is initialized to 0 on the pad
 */
 double getVelocity(void) {
 	time_t start,end;
-	double alt,theta,velocity;
+	double acc_first,acc_second;
 	start = time(NULL); //get begin time
-	alt = baro.getAltitude();//get first altitude reading 
+	acc_first = getAcceleration();//get first acceleration
 	delay(100);//delay for 100ms
-	alt = baro.getAltitude()-alt;//2nd reading - 1st reading is the difference in altitude over the time period
+	acc_second = getAcceleration();//get acceleration again
 	end = time(NULL);//get finish time
-	velocity=alt/(end-start);//velocity is distance/time
+	velocity=velocity + (end-start)*(acc_first+acc_second)/(2);//Vo + at
   return velocity;
 }
 
@@ -184,8 +245,10 @@ Using spherical coordinates, get angle of rocket to vertical
 event is a structure of type sensors_event_t which holds information from the
 gyro sensor used to derive the angles
 */
-double getAngleToVertical(sensors_event_t event) {
+double getAngleToVertical(void){
   // hardware read
+	sensors_event_t event;//orient event
+	bno.getEvent(&event, Adafruit_BNO055::VECTOR_GYROSCOPE);//get orientation
   double x=event.orientation.x;//get angle to x
   double y=event.orientation.y;//get angle to y
 	
